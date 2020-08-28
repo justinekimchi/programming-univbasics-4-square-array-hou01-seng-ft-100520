317db2ffb7e4f5016d0bad48f342fb2230447281
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-08-28 19:39:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -26,4 +26,12 @@ def square_array(numbers)
     new_array=[]
 end 
 
-
+def square_array(numbers)
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
